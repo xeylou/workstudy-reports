0e0c9dedda5a7a9cc72757cc27251140846eb6ea
xeylou <xeylou.pro@gmail.com>
2024-05-08 17:27:48 +0200

    speed

---
+++ v2/adehu_aditu_activity_report_2023-10-30_2023-11-17/compile.sh
@@ -0,0 +1,2 @@
+pdflatex --interaction nonstopmode main.tex
+mv main.pdf adehu_aditu_activity_report_2023-10-30_2023-11-17.pdf
---
+++ v2/adehu_aditu_activity_report_2024-02-12_2024-03-08/compile.sh
@@ -0,0 +1,3 @@
+pdflatex --interaction nonstopmode main.tex
+# pdflatex main.tex
+mv main.pdf adehu_aditu_activity_report_2024-02-12_2024-03-08.pdf
